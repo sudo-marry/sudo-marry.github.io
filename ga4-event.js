62f375157712f424bf525ce5e2b99b9ac4382be0
zerda217 <zerda@korea.ac.kr>
2026-04-13 15:23:23 +0900

    gtag 업데이트

---
+++ ga4-event.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var copyAddressButton = document.getElementById('copyAddressButton');
+  var callGroom = document.getElementById('callGroom');
+  var callBride = document.getElementById('callBride');
+  var copyGroomBank = document.getElementById('copyGroomBank');
+  var copyBrideBank = document.getElementById('copyBrideBank');
+
+  if (copyAddressButton) {
+    copyAddressButton.addEventListener('click', function() {
+      gtag('event', 'copy_address', {
+        'event_category': 'copy_address',
+        'event_label': '주소 복사 버튼 클릭'
+      });
+    });
+  }
+
+  if (callGroom) {
+    callGroom.addEventListener('click', function() {
+      gtag('event', 'call_groom', {
+        'event_category': 'copy_call',
+        'event_label': '신랑 전화 버튼 클릭'
+      });
+    });
+  }
+
+  if (callBride) {
+    callBride.addEventListener('click', function() {
+      gtag('event', 'call_bride', {
+        'event_category': 'copy_call',
+        'event_label': '신부 전화 버튼 클릭'
+      });
+    });
+  }
+
+  if (copyGroomBank) {
+    copyGroomBank.addEventListener('click', function() {
+      gtag('event', 'copy_groom_bank', {
+        'event_category': 'copy_bank',
+        'event_label': '신랑 계좌 복사 버튼 클릭'
+      });
+    });
+  }
+  if (copyBrideBank) {
+    copyBrideBank.addEventListener('click', function() {
+      gtag('event', 'copy_bride_bank', {
+        'event_category': 'copy_bank',
+        'event_label': '신부 계좌 복사 버튼 클릭'
+      });
+    });
+  }
+});
