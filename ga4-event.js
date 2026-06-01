0836c3d3426c648be73ee564888eed29670c1ba9
zerda217 <zerda@korea.ac.kr>
2026-06-01 17:59:18 +0900

    ga event update

--- ga4-event.js
+++ ga4-event.js
@@ -4,6 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
   var callBride = document.getElementById('callBride');
   var copyGroomBank = document.getElementById('copyGroomBank');
   var copyBrideBank = document.getElementById('copyBrideBank');
+  var copyBrideFatherBank = document.getElementById('copyBrideFatherBank');
+  var copyBrideMotherBank = document.getElementById('copyBrideMotherBank');
+  var viewPhoto = document.getElementById('viewPhoto');
 
   if (copyAddressButton) {
     copyAddressButton.addEventListener('click', function() {
@@ -45,6 +48,31 @@ document.addEventListener('DOMContentLoaded', function() {
       gtag('event', 'copy_bride_bank', {
         'event_category': 'copy_bank',
         'event_label': '신부 계좌 복사 버튼 클릭'
+      });
+    });
+  }
+  if (copyBrideFatherBank) {
+    copyBrideFatherBank.addEventListener('click', function() {
+      gtag('event', 'copy_bride_father_bank', {
+        'event_category': 'copy_bank',
+        'event_label': '신부 부 계좌 복사 버튼 클릭'
+      });
+    });
+  }
+  if (copyBrideMotherBank) {
+    copyBrideMotherBank.addEventListener('click', function() {
+      gtag('event', 'copy_bride_mother_bank', {
+        'event_category': 'copy_bank',
+        'event_label': '신부 모 계좌 복사 버튼 클릭'
+      });
+    });
+  }
+
+  if (viewPhoto) {
+    viewPhoto.addEventListener('click', function() {
+      gtag('event', 'view_photo', {
+        'event_category': 'view_photo',
+        'event_label': '사진 보기'
       });
     });
   }
